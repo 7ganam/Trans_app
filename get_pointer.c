fed7af1085efcb40f626984f6edf2859900ee8ff
7ganam <7ganam@gmail.com>
2018-07-29 22:20:38 +0200

    turned the code into c++ and compiled with g++

--- get_pointer.c
+++ get_pointer.c
@@ -12,7 +12,7 @@ int main(void) {
     Window *root_windows;
     Window window_returned;
     int root_x, root_y;
-    int win_x, win_y;
+    int win_x , win_y;
     unsigned int mask_return;
 
     Display *display = XOpenDisplay(NULL);
@@ -20,16 +20,15 @@ int main(void) {
 
 
 
-    root_windows = malloc(sizeof(Window) * 1);
-    
-   
-    root_windows = XRootWindow(display, 0);
+
+    root_windows = new(Window);
+    *root_windows = XRootWindow(display, 0);
 
     
     while(1)
     {
         sleep(1);
-        result = XQueryPointer(display, root_windows, &window_returned,  &window_returned, &root_x, &root_y, &win_x, &win_y,  &mask_return);
+        result = XQueryPointer(display, *root_windows, &window_returned,  &window_returned, &root_x, &root_y, &win_x, &win_y,  &mask_return);
         printf("Mouse is at (%d,%d)\n", root_x, root_y);
     }
     
